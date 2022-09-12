a5154ea3f342561488dd9d3e30154029ced2c2ed
Pedro Melo <pedromelo1800@gmail.com>
2022-09-12 05:05:41 -0300

    Adequações para funcionamento conjunto

--- Projeto/src/projeto/Resultado.java
+++ Projeto/src/projeto/Resultado.java
@@ -11,7 +11,7 @@ package projeto;
 public class Resultado {
     String data;
     int pontuacao;
-    Usuario usuario = new Usuario();
+    //Usuario usuario = new Usuario();
 
     public String getData() {
         return data;
@@ -29,13 +29,13 @@ public class Resultado {
         this.pontuacao = pontuacao;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
+//    public Usuario getUsuario() {
+//        return usuario;
+//    }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
+//    public void setUsuario(Usuario usuario) {
+//        this.usuario = usuario;
+//    }
     
     
     
--- Projeto/src/telas/Principal.java
+++ Projeto/src/telas/Principal.java
@@ -173,7 +173,7 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        new telaMatriz().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MousePressed
